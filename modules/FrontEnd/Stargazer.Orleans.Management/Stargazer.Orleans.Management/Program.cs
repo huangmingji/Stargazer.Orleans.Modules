2631cae22b3791a4094b28d665c58216df72c689
huangmingji <huangmingji@stargazer.tech>
2026-05-03 00:28:11 +0800

    feat(frontend): add Blazor WebAssembly management UI with MudBlazor
    - Add Blazor Server + WASM hosted app with MudBlazor components
    - Fix NavMenu relative URLs (add leading slashes, use NavLinkMatch.All)
    - Remove invalid [PersistentState] attribute from Weather.razor
    - Remove conflicting UseStatusCodePagesWithReExecute middleware

---
+++ modules/FrontEnd/Stargazer.Orleans.Management/Stargazer.Orleans.Management/Program.cs
@@ -0,0 +1,37 @@
+using MudBlazor.Services;
+using Stargazer.Orleans.Management.Client.Pages;
+using Stargazer.Orleans.Management.Components;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add MudBlazor services
+builder.Services.AddMudServices();
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveWebAssemblyComponents();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseWebAssemblyDebugging();
+}
+else
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+app.UseHttpsRedirection();
+
+
+app.UseAntiforgery();
+
+app.MapStaticAssets();
+app.MapRazorComponents<App>()
+    .AddInteractiveWebAssemblyRenderMode()
+    .AddAdditionalAssemblies(typeof(Stargazer.Orleans.Management.Client._Imports).Assembly);
+
+app.Run();
